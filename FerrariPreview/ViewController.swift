dfdf4dccca65a8f77893af5b709ae4248f755551
Steve Huang <shaobaihuang@gmail.com>
2019-11-24 11:01:27 +0800

    Initial Commit

---
+++ FerrariPreview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FerrariPreview
+//
+//  Created by ShaoPo Huang on 2019/11/24.
+//  Copyright © 2019 ShaoPo Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
